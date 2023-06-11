22f982bca1b18a11ce6e03f73b1f6df787e5b4d3
Volcomix <m.santo13@hotmail.com>
2023-06-11 15:10:07 -0600

    started code for server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-const 
+const mysql = require ("mysql2");
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        // MySQL username,
+        user: "root",
+        // TODO: Add MySQL password here
+        password: "1234",
+        database: "employeeTracker_db",
+    },
+);
