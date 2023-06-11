896339a29e1ded10941dcef1b4013d754d4e7a2f
Volcomix <m.santo13@hotmail.com>
2023-06-11 15:04:45 -0600

    added data to seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,21 @@ CREATE DATABASE employeeTracker_db;
 USE employeeTracker_db;
 
 CREATE TABLE department (
-    id INT NOT NULL 
+    id INT NOT NULL PRIMARY KEY,
     name VARCHAR(30)
 );
 
-CREATE TABLE role (
-    id INT NOT NULL
-    title VARCHAR(30)
-    salary DECIMAL
+CREATE TABLE roles (
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
     department_id INT NOT NULL
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL
-    first_name VARCHAR(30)
-    last_name VARCHAR (30)
-    role_id INT NOT NULL
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR (30),
+    role_id INT NOT NULL,
     manager_id INT NOT NULL
 );
---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO deparment (name)
+VALUES  ("Accounting"),
+        ("Human Resources"),
+        ("OPS");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Manager", 98000, 1),
+       ("Team Lead", 60000, 2),
+       ("Associate", 35000,3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Nam-joon", "Kim", 5, 1),
+       ("Seok-jin", "Kim", 10, 2),
+       ("Yoon-gi", "Min", 20, 3);
